8b34b81bb756b215424163f821d85da1fe440498
lucas.scherolt <lucas.scherolt@NDD-NOT-7537>
2024-10-15 22:37:59 -0300

    feat: dividindo o texto em partes

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from services import openai_service, elevenlabs_service, video_manager
 from prompts import prompt_service
 import re
 
-def generate_image_descriptions(imagePromptToCreate):
-    imagePromptCreated = openai_service.generate_narration(imagePromptToCreate)
-    return  [re.sub(r'^\d+\.\s*', '', item.strip()) for item in imagePromptCreated.content.split('\n') if item]
+def splitPrompt(prompt):
+    result = openai_service.generate_narration(prompt)
+    return [re.sub(r'^\d+\.\s*', '', item.strip()) for item in result.content.split('\n') if item]
 
 def setNarrationText():
     textTheme = input("Theme of the text (ex. Technology, The History of Art): ")
@@ -27,7 +27,7 @@ def setAudio(createdNarrationText):
 def setImages(createdNarrationText):
     imagePromptToCreate = prompt_service.get_images_prompt(createdNarrationText)
     
-    imageDescriptions = generate_image_descriptions(imagePromptToCreate)
+    imageDescriptions = splitPrompt(imagePromptToCreate)
     imageUrlList = []
 
     for i, prompt in enumerate(imageDescriptions):
@@ -37,13 +37,34 @@ def setImages(createdNarrationText):
     print("Images descriptions generated")
     return imageUrlList
 
+def divideNarration(createdNarrationText):
+    splitedNarrationPromptToCreate = prompt_service.get_divided_narration_prompt(createdNarrationText)
+    
+    splitedNarration = splitPrompt(splitedNarrationPromptToCreate)
+
+    for i, prompt in enumerate(splitedNarration):
+        url = openai_service.generate_image(prompt)
+
+# def cleanSplitedNarration(splitedNarration):
+#     # Nova lista para armazenar as partes válidas (sem **)
+#     clean_narration = []
+    
+#     # Percorre cada item da lista e verifica se contém "**"
+#     for i, prompt in enumerate(splitedNarration):
+#         if '**' not in prompt:  # Verifica se não há metadados como **Tema** ou **Estilo**
+#             clean_narration.append(prompt)  # Adiciona apenas as partes válidas
+    
+#     return clean_narration
+
 def setVideo(imageUrlList):
     video_manager.create_video(imageUrlList)
 
 def main():
     createdNarrationText = setNarrationText()
+    divideNarration(createdNarrationText);
 
-    setAudio(createdNarrationText)
+
+    # setAudio(createdNarrationText)
 
     imageUrlList = setImages(createdNarrationText)
 
--- prompts/prompt_service.py
+++ prompts/prompt_service.py
@@ -1,14 +1,26 @@
+def get_divided_narration_prompt(createdNarration):
+    return f"""
+    Based on the following text: "{createdNarration}"
+    Please divide this text into sections that can be transformed into images. Each section should correspond to a specific part of the text that can be visualized as an image. Return the original text divided into parts that represent these sections.
+    The format should be:
+    1. Text section 1
+    2. Text section 2
+    3. Text section 3
+    Do not include descriptions or additional text. Ensure the output follows the order of the original text, and when combined, they should represent the entire text exactly as it was.
+    """
+
 def get_narration_prompt(theme, 
                          objective='Educate', 
                          emotion='Curiosity', 
                          narrationStyle='Casual', 
                          language='Portugues - BR', 
-                         lenght='Short'):
+                         length='Short'):
     return f"""
-            Create text on the theme {theme}. The objective of this text is {objective},
-            and it should evoke the emotion of {emotion} in the readers.
-            The text style should be {narrationStyle}, and it should be written in {language}.
-            Make the lenght ot the text {lenght}.
+            Create text on the theme "{theme}". The objective of this text is "{objective}",
+            and it should evoke the emotion of "{emotion}" in the readers.
+            The text style should be "{narrationStyle}", and it should be written in "{language}".
+            Make the length of the text "{length}".
+            Important: Do not include any metadata, such as **Title**, **Subtitle**, **Theme**, or **Style**. Only generate the text itself, without introductions, headings, or any other extra elements. The result should be a continuous, flowing narrative.
             """
 
 def get_images_prompt(createdNarration):
